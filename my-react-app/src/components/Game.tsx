0ab694e7b85296d0ef623bbc00c7c4e9ef8ed46c
AmeliaBigler <amaebigler@gmail.com>
2024-09-28 20:29:42 -0500

    wip onclick active cards - ugly but works

--- my-react-app/src/components/Game.tsx
+++ my-react-app/src/components/Game.tsx
@@ -4,43 +4,54 @@ import axios from 'axios';
 import BottomNav from './BottomNav';
 
 const Game: FC = () => {
+	// Hooks
+	const [loading, setLoading] = useState(true);
+	const [persons, setPersons] = useState([]);
+	const [personCardClick, updatePersonCardClick] = useState(null);
+	const [activePersons, updateActivePersons] = useState<string[]>([]);
 
-  const [loading, setLoading] = useState(true);
-  const [persons, setPersons] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get('http://localhost:8080/persons/board');
+				setPersons(response.data);
+				updateActivePersons(response.data.map((i: { id: any }) => i.id));
+			} finally {
+				setLoading(false);
+			}
+		};
 
-  useEffect(() => {
-    const fetchData = async () => {
-        try {
-            const response = await axios.get('http://localhost:8080/persons/board');
-            setPersons(response.data);
-        } finally {
-            setLoading(false);
-        }
-    };
+		fetchData();
+	}, []);
 
-    fetchData();
-}, []);
+	useEffect(() => {
+		console.log(activePersons);
+		if (activePersons.includes(personCardClick))
+			updateActivePersons(activePersons.filter((i) => i !== personCardClick));
+		else updateActivePersons(activePersons.concat(personCardClick));
+    updatePersonCardClick(null);
+	}, [personCardClick]);
 
-if (loading) {
-  return <div>Loading</div>
-} else {
-  return(
-    <div className='px-10 grid grid-rows-4 grid-cols-6 gap-3'>
-      {/* <PersonCard name='sam' image='placeholder' isActive={false}/>
-      <PersonCard name='sam' image='placeholder' isActive={true}/>
-      <PersonCard name='sam' image='placeholder' isActive={true}/>
-      <PersonCard name='sam' image='placeholder' isActive={true}/>
-      <PersonCard name='sam' image='placeholder' isActive={true}/>
-      <PersonCard name='sam' image='placeholder' isActive={true}/>
-      <PersonCard name='sam' image='placeholder' isActive={true}/>
-      <PersonCard name='sam' image='placeholder' isActive={true}/> */}
-      {persons.map(person => {
-        return <PersonCard name={person.name} image={person.imageUrl} isActive={true} />
-      })}
-      <BottomNav />
-    </div>
-  )
-}
-}
+	if (loading) {
+		return <div>Loading</div>;
+	} else {
+		return (
+			<div className='px-10 grid grid-rows-4 grid-cols-6 gap-3'>
+				{persons.map((person) => {
+					return (
+						<PersonCard
+							personId={person.id}
+							name={person.name}
+							image={person.imageUrl}
+							isActive={activePersons.includes(person.id.toString())}
+							onClick={updatePersonCardClick}
+						/>
+					);
+				})}
+				<BottomNav />
+			</div>
+		);
+	}
+};
 
 export default Game;
--- my-react-app/src/components/PersonCard.tsx
+++ my-react-app/src/components/PersonCard.tsx
@@ -1,16 +1,18 @@
 import { FC } from 'react';
 
 interface PersonCardProps {
+  personId: string;
 	name: string;
 	image: any;
-	isActive: Boolean;
+  isActive: Boolean;
+	onClick: any;
 }
 
-const PersonCard: FC<PersonCardProps> = ({ name, image, isActive }) => {
+const PersonCard: FC<PersonCardProps> = ({ personId, name, image, isActive, onClick }) => {
 	const x = <div>x</div>;
 
 	return (
-		<div className='w-24 h-32 bg-blue-300 rounded-md flex flex-col'>
+		<div className='w-24 h-32 bg-blue-300 rounded-md flex flex-col' onClick={() => onClick(personId)}>
 			<div className='h-4/5 flex justify-center'>{isActive ? <img src={image}></img> : x}</div>
 			<div className='h-1/5 flex justify-center bg-slate-50'>{name}</div>
 		</div>
