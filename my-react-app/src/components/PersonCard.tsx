5f767fefa3f87e6b4cc1e9e31c52203b8cf7be1e
Hunter Ehrenfeld <hehrenfeld@fanthreesixty.com>
2024-09-28 21:14:29 -0500

    Update PersonCard.tsx

--- my-react-app/src/components/PersonCard.tsx
+++ my-react-app/src/components/PersonCard.tsx
@@ -1,22 +1,58 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { pinata } from '../utils/pinata.config';
 
 interface PersonCardProps {
   personId: string;
-	name: string;
-	image: any;
+  name: string;
+  cid: any;
   isActive: Boolean;
-	onClick: any;
+  onClick: any;
 }
 
-const PersonCard: FC<PersonCardProps> = ({ personId, name, image, isActive, onClick }) => {
-	const x = <div>x</div>;
+const PersonCard: FC<PersonCardProps> = ({
+  personId,
+  name,
+  cid,
+  isActive,
+  onClick,
+}) => {
+  const [imageUrl, setImageUrl] = useState('');
+  // Function to handle fetching the signed URL and setting the imageUrl
+  const fetchImage = async () => {
+    if (!cid) {
+      console.error('Invalid CID!');
+      return;
+    }
 
-	return (
-		<div className='w-24 h-32 bg-blue-300 rounded-md flex flex-col' onClick={() => onClick(personId)}>
-			<div className='h-4/5 flex justify-center'>{isActive ? <img src={image}></img> : x}</div>
-			<div className='h-1/5 flex justify-center bg-slate-50'>{name}</div>
-		</div>
-	);
+    try {
+      const signedUrl = await pinata.gateways.createSignedURL({
+        cid: cid,
+        expires: 50, // 50 minutes
+      });
+      setImageUrl(signedUrl);
+    } catch (error) {
+      console.error('Error fetching image:', error);
+    }
+  };
+
+  // Use `useEffect` to fetch the image when the component mounts or CID changes
+  useEffect(() => {
+    if (cid && !imageUrl) {
+      fetchImage();
+    }
+  }, [cid]);
+
+  return (
+    <div
+      className='w-24 h-32 bg-blue-300 rounded-md flex flex-col'
+      onClick={() => onClick(personId)}
+    >
+      <div className='h-4/5 flex justify-center'>
+        {isActive ? <img src={imageUrl || undefined} alt={name} /> : 'x'}
+      </div>
+      <div className='h-1/5 flex justify-center bg-slate-50'>{name}</div>
+    </div>
+  );
 };
 
 export default PersonCard;
